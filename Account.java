bdba831229655f9a03d209cd7c65c75aed3af0ad
Sara Pojaku <115804147+sarapojaku@users.noreply.github.com>
2025-08-30 15:33:24 +0200

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,13 +1,43 @@
+import java.util.ArrayList;
+
 public class Account {
-    protected String username;
-    protected String password;
+    private String username;
+    private String password;
+    private ArrayList<Book> borrowedBooks;
 
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
+        this.borrowedBooks = new ArrayList<>();
     }
 
-        public boolean login(String username, String password) {
+    public boolean login(String username, String password) {
         return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public void borrowBook(Book book) {
+        if (book.isAvailable) {
+            book.borrowBook();
+            borrowedBooks.add(book);
+        } else {
+            System.out.println(book.title + " is not available.");
+        }
+    }
+
+    public void returnBook(Book book) {
+        if (borrowedBooks.contains(book)) {
+            book.returnBook();
+            borrowedBooks.remove(book);
+        } else {
+            System.out.println("You did not borrow this book.");
+        }
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
